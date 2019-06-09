0e83ce34bcb34671aeb1fee37dd8aa7e839287ba
TiaoTiao87 <47874872+TiaoTiao87@users.noreply.github.com>
2019-06-09 16:32:58 +0800

    Update cal.js

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,43 +1,9 @@
 <script>
-var temp = 0;
-var strcal = 0;
-function numberBtn(str){
-	if(document.getElementById("box").value==0)
-		{
-			document.getElementById("box").value = str;
-		}
-	else
-		{
-			document.getElementById("box").value += str;
-		}
-	}
-function cal(str){
-	equal();
-	valueA = document.getElementById("box").value;
-	strcal = str;
-	document.getElementById("box").value = 0;
-	}
-function equal(){
-	switch(strcal){
-		case '+':
-			document.getElementById("box").value = parseFloat(valueA)+parseFloat(document.getElementById("box").value);
-			break;
-		case '-':
-			document.getElementById("box").value = parseFloat(valueA)-parseFloat(document.getElementById("box").value);
-			break;
-		case '*':
-			document.getElementById("box").value = parseFloat(valueA)*parseFloat(document.getElementById("box").value);
-			break;
-		case '/':
-			document.getElementById("box").value = parseFloat(valueA)/parseFloat(document.getElementById("box").value);
-			break;
-		default:
-			break;
-		}
-}
-function BtnC(){
-	temp = 0;
-	strcal = 0;
-	document.getElementById("box").value = '0';
-	} 
+  function calculate() {
+    var a = document.getElementById('a')
+    var op = document.getElementById('op')
+    var b = document.getElementById('b')
+    var result = document.getElementById('result')
+    result.innerText = eval(a.value + op.value + b.value)
+  }
 </script>
